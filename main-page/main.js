5fc7d55be05009ac8b57fdb62680ee302d4f5ea4
Sujal-ap <ushabalmiki8@gmail.com>
2024-04-22 23:29:24 +0530

    sirf shreys

--- main-page/main.js
+++ main-page/main.js
@@ -2,7 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signOut } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 const firebaseConfig = {
-    // Your Firebase configuration
+    apiKey: "AIzaSyCEXDTA-9yOtQHfTqzl0c3g1aNLzY4_3pg",
+    authDomain: "auth-df231.firebaseapp.com",
+    projectId: "auth-df231",
+    storageBucket: "auth-df231.appspot.com",
+    messagingSenderId: "459558142200",
+    appId: "1:459558142200:web:dc776e693fd9d333bd7b0d"    
 };
 
 const app = initializeApp(firebaseConfig);
